35543b651ac524831b777312e9cf5f4b08b7a09f
Antonela Ledesma <ledesmaantonela20@gmail.com>
2023-04-19 08:07:22 -0300

    exercise correction

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,10 +1,8 @@
 //Haga una función que reciba un arreglo y un número. Tenemos que sumarle dicho número a cada elemento del arreglo y devolver el arreglo modificado.
 
 const modifyArrayOfNumbers = (array , number) => {
-    let modifiedArrayOfNumbers = array.map(function(array) {
-        return array + number
-    })
-    return console.log(modifiedArrayOfNumbers)
+    let modifiedArrayOfNumbers = array.map(oldNumber=> oldNumber + number) 
+    console.log(modifiedArrayOfNumbers)
 }
 
 const arrayOfNumbers = [ 3, 5, 7, 37, 45, 99]
