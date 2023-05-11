49257abdba28de5cd5618265f19f59716a6ee169
Antonela Ledesma <ledesmaantonela20@gmail.com>
2023-05-11 18:21:36 -0300

    add exercise correction

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,21 +1,21 @@
 //Haga una función que solo reciba arreglos con más de 5 elementos y luego elimine los últimos 3. En caso de recibir un arreglo de menos de 5 elementos, mostrar en pantalla un mensaje de error.
 
-const deleteLastThreeElements = (array) => { 
-    if (array.length < 5 ){
-        console.log("ERROR! : El array debe tener 5 o más elementos")
-        return
-    }
+const deleteLastThreeElements = (array) => {
+  if (array.length < 5) {
+    console.log("ERROR! : El array debe tener 5 o más elementos");
+    return;
+  }
 
-    array.pop()
-    array.pop()
-    array.pop()
-    console.log(array)
-}
+  array.pop();
+  array.pop();
+  array.pop();
+  console.log(array);
+};
 
-const array1 = [ 1, 3, 5, 7, 9, 23, 28]
+const arrayOfNumbers = [1, 3, 5, 7, 9, 23, 28];
 
-deleteLastThreeElements(array1)
+deleteLastThreeElements(arrayOfNumbers);
 
-const array2 = ["pepito", "manito", "cosito", "osito"]
+const cities = ["Buenos Aires", "Seúl", "París", "Tokio"];
 
-deleteLastThreeElements(array2)
+deleteLastThreeElements(cities);
